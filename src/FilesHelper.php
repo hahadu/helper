aa6bd104fbcb323f5c2f6b805b3727d872daf0d7
hahadu <mhyzhu@163.com>
2022-09-06 00:57:34 +0800

    update

--- src/FilesHelper.php
+++ src/FilesHelper.php
@@ -152,34 +152,34 @@ class FilesHelper
         // 取文件后缀名
         $str= self::get_file_ext($file);
         // 图片格式
-        $image=array('webp','jpg','png','ico','bmp','gif','tif','pcx','tga','bmp','pxc','tiff','jpeg','exif','fpx','svg','psd','cdr','pcd','dxf','ufo','eps','ai','hdri');
+        $image=['webp','jpg','png','ico','bmp','gif','tif','pcx','tga','bmp','pxc','tiff','jpeg','exif','fpx','svg','psd','cdr','pcd','dxf','ufo','eps','ai','hdri'];
         // 视频格式
-        $video=array('mp4','avi','3gp','rmvb','gif','wmv','mkv','mpg','vob','mov','flv','swf','mp3','ape','wma','aac','mmf','amr','m4a','m4r','ogg','wav','wavpack');
+        $video=['mp4','avi','3gp','rmvb','gif','wmv','mkv','mpg','vob','mov','flv','swf','mp3','ape','wma','aac','mmf','amr','m4a','m4r','ogg','wav','wavpack'];
         // 压缩格式
-        $zip=array('rar','zip','tar','cab','uue','jar','iso','z','7-zip','ace','lzh','arj','gzip','bz2','tz');
+        $zip=['rar','zip','tar','cab','uue','jar','iso','z','7-zip','ace','lzh','arj','gzip','bz2','tz'];
         // 文档格式
-        $text=array('exe','doc','ppt','xls','wps','txt','lrc','wfs','torrent','html','htm','java','js','css','less','php','pdf','pps','host','box','docx','word','perfect','dot','dsf','efe','ini','json','lnk','log','msi','ost','pcs','tmp','xlsb');
+        $text=['exe','doc','ppt','xls','wps','txt','lrc','wfs','torrent','html','htm','java','js','css','less','php','pdf','pps','host','box','docx','word','perfect','dot','dsf','efe','ini','json','lnk','log','msi','ost','pcs','tmp','xlsb'];
         // 匹配不同的结果
         switch ($str) {
-            case in_array($str, $image):
+            case ArrayHelper::in_iarray($str, $image):
                 $result = [
                     'file_type'=>'image',
                     'file_ext' =>$str,
                 ];
                 break;
-            case in_array($str, $video):
+            case ArrayHelper::in_iarray($str, $video):
                 $result = [
                     'file_type'=>'video',
                     'file_ext' =>$str,
                 ];
                 break;
-            case in_array($str, $zip):
+            case ArrayHelper::in_iarray($str, $zip):
                 $result = [
                     'file_type'=>'zip',
                     'file_ext' =>$str,
                 ];
                 break;
-            case in_array($str, $text):
+            case ArrayHelper::in_iarray($str, $text):
                 $result = [
                     'file_type'=>'text',
                     'file_ext' =>$str,
